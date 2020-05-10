fef853a54d0ee7835b5c0b065048d1a8148c096d
Karen Kuzmiak <lamar.clapham@gmail.com>
2020-05-10 11:52:41 -0700

    Activity 41 completed

--- js/objects.js
+++ js/objects.js
@@ -1,23 +1,30 @@
 // Objects in JS have properties and things they can do(methods)
 
 // Object literal notation
+// const blogs = [
+//     { title: 'why mac and cheese rules', likes: 30 },
+//     { title: 'wTen things to make with marmit', likes: 50 }
+// ]
 
 let user = {
     name: 'crystal',
     age: 30,
     email: 'cyrstal@gmail.com',
     location: 'Berlin',
-    blogs: ['why mac and chsess rules','10 things to makewith marmit'],
-    login: function() { // This is method.A function designated in an object
+    blogs: [
+        { title: 'why mac and cheese rules', likes: 30 },
+        { title: 'wTen things to make with marmit', likes: 50 }
+    ],
+    login: function () { // This is method.A function designated in an object
         console.log("The user logged in")
     },
-    logout: function() { // This is method.A function designated in an object
+    logout: function () { // This is method.A function designated in an object
         console.log("The user logged out")
     },
-    logBlogs: function() { // This is method.A function designated in an object
+    logBlogs: function () { // This is method.A function designated in an object
         console.log("This user has written about the following things")
         this.blogs.forEach(blog => {
-            console.log(blog);
+            console.log(blog.title, blog.likes);
         })
     }
 };
